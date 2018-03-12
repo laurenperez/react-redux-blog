64e63e1023b264e31271ef28c3f42b7157105693
laurenperez <laurenashleymarks@gmail.com>
2018-03-12 14:37:51 -0700

    added notes on store and actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,18 @@ import { ADD_ARTICLE } from "../constants/action-types";
  export const addArticle = article => (
   { type: ADD_ARTICLE, payload: article }
 )
+
+
+//Notes on Actions!
+
+//the only way to change state is to send a signal to the Store
+//dispatching an action is the process of sending that signal
+
+//Actions are objects that need 2 things - type and payload
+// ex:
+// {
+//   type: "ACTION_NAME_HERE",
+//   payload: thingThatYouWantToSendToStore
+// }
+
+//Naming your action - action names should be descriptive and reflect what they do (or what the user is doing)
--- src/store/index.js
+++ src/store/index.js
@@ -4,3 +4,19 @@ import rootReducer from "../reducers/index";
 const store = createStore(rootReducer);
 
 export default store;
+
+
+//Notes on Store!
+
+//the store is like the brain of your app = all versions of state are kept here like memories
+
+//methods for accessing store:
+
+//store.getState() - returns an object of your current store
+//store.getState() - { articles: Array(0) }
+
+//store.dispatch() - pass in a payload to the "create action" function, it returns a new action obj to be dispatched
+//store.dispatch( addArticle( { title: "article", id:1 }))
+
+//store.subscribe() - listens for store updates, takes a callback that does something when the store is updated
+//store.subscribe(() => { console.log("redux is happening!")} )
